6272b620b77e12a1c62ab4f32d05d3d6fe2945f2
Thiago Figueiredo <tsoaresfigueiredo@gmail.com>
2018-06-09 19:46:59 -0400

    Finalizado sem estilo

--- pessoa.js
+++ pessoa.js
@@ -11,12 +11,29 @@ class Pessoa {
     adicionar() { //Adiciona uma pessoa na lista
         let nome = document.getElementById("nome").value;
         let sexo = document.querySelector("[name=sexo]:checked").value;
+        let btnM = document.getElementById("btnM");
+        let btnF = document.getElementById("btnF");
+        let btnAdd = document.getElementById("btnAdicionar");
 
         let pessoa = {
             nome: nome,
             sexo: sexo
         };
 
+        if (sexo == "M") {
+            this.qtdSexoM++;
+            btnM.innerText = "";
+            btnM.innerText = "Listar homens (" + this.qtdSexoM + ")";
+        } else {
+            this.qtdSexoF++;
+            btnF.innerText = "";
+            btnF.innerText = "Listar mulheres (" + this.qtdSexoF + ")";
+        }
+
+        if ((this.qtdSexoM + this.qtdSexoF) > 0) {
+            let total = this.qtdSexoM + this.qtdSexoF;
+            btnAdd.innerText = "Adicionar (" + total + ")";
+        }
         this.pessoas.push(pessoa);
     } //Fim de adicionar()
 
@@ -32,21 +49,34 @@ class Pessoa {
         }
     }
 
+    montarLista(vet) {
+        let divLista = document.getElementById("lista");
+        divLista.innerText = "";
+
+        for (let i = 0; i < vet.length; i++) {
+            let divPessoa = document.createElement("div");
+            divPessoa.innerText = (i+1) + " - " + vet[i].nome;
+            divLista.appendChild(divPessoa);
+        }
+    }
+
     listarSexoMasculino() { //Mostra na tela as pessoas do sexo Masculino
+        this.sexoM = [];
         this.verificarSexoDasPessoas();
 
         if (this.qtdSexoM > 0) {
-            console.log(this.sexoM);
+            this.montarLista(this.sexoM);
         } else {
             alert("Não existem Homens cadastrados");
         }
     }
 
     listarSexoFeminino() { //Mostra na tela as pessoas do sexo Feminino
+        this.sexoF = [];
         this.verificarSexoDasPessoas();
 
         if (this.qtdSexoF > 0) {
-            console.log(this.sexoF);
+            this.montarLista(this.sexoF);
         } else {
             alert("Não existem Mulheres cadastradas");
         }
